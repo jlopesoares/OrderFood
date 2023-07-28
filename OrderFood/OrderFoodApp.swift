c12e4667d8ee180323d0239d50bfceed55fc121f
João Pedro Soares <joao.p.soares93@gmail.com>
2023-07-28 23:11:53 +0100

    Initial setup

---
+++ OrderFood/OrderFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderFoodApp.swift
+//  OrderFood
+//
+//  Created by João Pedro Soares on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct OrderFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
